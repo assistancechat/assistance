34b1896d1d93123b81acef45af9eb37b8b98e6e0
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-12-21 09:07:57 +1100

    hello world working

--- src/python/isacareers/__main__.py
+++ src/python/isacareers/__main__.py
@@ -14,16 +14,21 @@
 
 from typing import Union
 
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run("__main__:app", port=8080, log_level="info")
