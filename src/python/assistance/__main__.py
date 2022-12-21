d3bee5d8162496531e845d792635ffb9e88a7acf
SimonBiggs <simon@radiotherapy.ai>
2022-12-21 22:09:04 +0000

    move to api

--- src/python/assistance/__main__.py
+++ src/python/assistance/__main__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .main import main
+from assistance.api.main import main
 
 if __name__ == "__main__":
     main()
