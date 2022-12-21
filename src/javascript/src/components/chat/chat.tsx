22b9007f45bf61105430f6510e0ab7d74c2d0361
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-12-21 14:29:36 +1100

    deploy to gh-pages

--- src/javascript/src/components/chat/chat.tsx
+++ src/javascript/src/components/chat/chat.tsx
@@ -27,54 +27,6 @@ export default component$((props: {conversation: string[]}) => {
                         </div>
                     )
                 })}
-
-                <div class="flex justify-start mb-4">
-                    <img
-                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                    class="object-cover h-8 w-8 rounded-full"
-                    alt=""
-                    />
-                    <div
-                    class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
-                    >
-                    Hi there! Can you tell me a little about yourself and
-                    what your skills and interests are?
-                    </div>
-                </div>
-                <div class="flex justify-end mb-4">
-                    <div>
-                    <div
-                        class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
-                    >
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                        Magnam, repudiandae.
-                    </div>
-
-                    <div
-                        class="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
-                    >
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Debitis, reiciendis!
-                    </div>
-                    </div>
-                    <img
-                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                    class="object-cover h-8 w-8 rounded-full"
-                    alt=""
-                    />
-                </div>
-                <div class="flex justify-start mb-4">
-                    <img
-                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                    class="object-cover h-8 w-8 rounded-full"
-                    alt=""
-                    />
-                    <div
-                    class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
-                    >
-                    happy holiday guys!
-                    </div>
-                </div>
             </div>
             <div class="py-5">
                 <input
