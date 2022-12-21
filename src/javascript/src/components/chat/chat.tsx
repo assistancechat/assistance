6b87872634ca86b1ab3f473072f4239903083fd0
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-12-21 14:24:40 +1100

    fix image order

--- src/javascript/src/components/chat/chat.tsx
+++ src/javascript/src/components/chat/chat.tsx
@@ -1,27 +1,68 @@
 import { component$ } from "@builder.io/qwik";
 
 export default component$((props: {conversation: string[]}) => {
+
+
+
+
   return (
     <div class="fixed right-10 bottom-10 w-96 z-40 shadow-2xl rounded-lg bg-white">
         <div class="w-full px-5 flex flex-col justify-between">
             <div class="mt-5">
-
                 {props.conversation.map((content, index) => {
                     return (
-                        <div class={`"flex ${index % 2 == 0 ? "justify-start" : "justify-end"} mb-4"`}>
-                                <img
-                                src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
-                                class="object-cover h-8 w-8 rounded-full"
-                                alt=""
-                                />
+                        <div class={`flex ${index % 2 == 0 ? "justify-start" : "justify-end"} mb-4`}>
+                            <img
+                            src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                            class="object-cover h-8 w-8 rounded-full"
+                            style={{order: `${index % 2 == 0 ? "1" : "3"}`}}
+                            alt=""
+                            />
                             <div
-                            class={`${index % 2 == 0 ? "bg-gray-400 ml-2 rounded-br-3xl rounded-tr-3xl rounded-tl-xl" : "bg-blue-400 mr-2 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl"} py-3 px-4 text-white`}
+                            class={`ml-2 py-3 px-4 text-white ${index % 2 == 0 ? "bg-gray-400 ml-2 rounded-br-3xl rounded-tr-3xl rounded-tl-xl" : "bg-blue-400 mr-2 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl"}`}
+                            style={{order: "2"}}
                             >
                                 {content}
                             </div>
                         </div>
                     )
                 })}
+
+                <div class="flex justify-start mb-4">
+                    <img
+                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                    class="object-cover h-8 w-8 rounded-full"
+                    alt=""
+                    />
+                    <div
+                    class="ml-2 py-3 px-4 bg-gray-400 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white"
+                    >
+                    Hi there! Can you tell me a little about yourself and
+                    what your skills and interests are?
+                    </div>
+                </div>
+                <div class="flex justify-end mb-4">
+                    <div>
+                    <div
+                        class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+                    >
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        Magnam, repudiandae.
+                    </div>
+
+                    <div
+                        class="mt-4 mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white"
+                    >
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Debitis, reiciendis!
+                    </div>
+                    </div>
+                    <img
+                    src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
+                    class="object-cover h-8 w-8 rounded-full"
+                    alt=""
+                    />
+                </div>
                 <div class="flex justify-start mb-4">
                     <img
                     src="https://source.unsplash.com/vpOeXr5wmR4/600x600"
