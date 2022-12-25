6890c5a24c802cf88da0020367b339af0e28d04a
Simon Biggs <simon.biggs@radiotherapy.ai>
2022-12-26 09:31:40 +1100

    update favicon

--- src/javascript/src/components/core/RouterHead.tsx
+++ src/javascript/src/components/core/RouterHead.tsx
@@ -14,7 +14,7 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={loc.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
 
       {head.meta.map((m) => (
         <meta {...m} />
