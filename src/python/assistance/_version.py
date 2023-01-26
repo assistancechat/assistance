475b2f86c038028f607e4d6bc0045bfc439a748f
Simon Biggs <simon.biggs@radiotherapy.ai>
2023-01-26 11:30:35 +1100

    get version dynamically

--- src/python/assistance/_version.py
+++ src/python/assistance/_version.py
@@ -12,4 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.1.1"
+from importlib import metadata
+
+__version__ = metadata.version(__package__)
