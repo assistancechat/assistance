024382482a71de11cd801db8fb568aca90fd108d
Kennie12345 <52986783+Kennie12345@users.noreply.github.com>
2023-01-30 11:19:22 +1100

    Counselling Splash Page v1
    A Counselling Splash Page demo for the assistance.chat platform
    
    Notes:
    - Do not use the Chat (function is still in development)
    
    Future adds:
    - Course search bar
    - Chat Functionality
    - Enrolment Forms

---
+++ examples/student.assistance.chat/src/components/GoogleEnrolmentForm.jsx
@@ -0,0 +1,16 @@
+function GoogleEnrolmentForm() {
+  return (
+    <iframe
+      src='https://forms.gle/bJohSSgcd2g61WKSA'
+      width='640'
+      height='800'
+      frameborder='0'
+      marginheight='0'
+      marginwidth='0'
+    >
+      Loading…
+    </iframe>
+  )
+}
+
+export default GoogleEnrolmentForm
---
+++ examples/student.assistance.chat/src/pages/ChatTest.tsx
@@ -0,0 +1,11 @@
+import ChatHeader from "@/components/ChatHeader"
+import CounsellingProfile1 from '../images/counsellingprofile1.png'
+import Chat from '../components/Chat';
+
+export default function ChatTest () {
+ return (
+    <>
+        <Chat />
+    </>
+ ) 
+}
