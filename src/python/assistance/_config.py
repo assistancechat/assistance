995bda4ba584581c773a2d59c15712b9347838c7
Simon Biggs <simon.biggs@radiotherapy.ai>
2023-01-31 14:35:08 +1100

    adjust store paths

--- src/python/assistance/_config.py
+++ src/python/assistance/_config.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 Assistance.Chat contributors
+# Copyright (C) 2023 Assistance.Chat contributors
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,14 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import pathlib
-
-LIB = pathlib.Path(__file__).parent
-
-STORE = pathlib.Path.home() / ".assistance"
-CONFIG = STORE / "config"
-SECRETS = CONFIG / "secrets"
-
-USERS = STORE / "users"
-RECORDS = STORE / "records"
