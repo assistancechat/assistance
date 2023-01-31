5be34b8f44f833611a283b2f91119c95652e2a44
Simon Biggs <simon.biggs@radiotherapy.ai>
2023-01-31 20:26:13 +1100

    pending question working

--- examples/student.assistance.chat/src/components/Hero.jsx
+++ examples/student.assistance.chat/src/components/Hero.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import { ArrowDownCircleIcon } from "@heroicons/react/24/solid";
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/outline";
 import Document from "@/pages/_document";
+
+import StartChatWithQuestionButton from "@/components/atoms/StartChatWithQuestionButton";
 
 const courseResources = {
   ImageBackground: {
@@ -15,7 +17,7 @@ const courseResources = {
   courseTitle: "COURSE RESOURCES",
   headLine: "Discover God's Purpose for you in Counselling",
   subHeading:
-    "Talk with our customer exerience officer to find the right course for you",
+    "Talk with our customer experience officer to find the right course for you",
   LearnButton: {
     text: "Study Christian Counselling",
     link: "https://www.ac.edu.au/healthbrochure",
@@ -64,20 +66,9 @@ export default function Hero() {
             </h3>
           </div>
 
-          <button
-            type="button"
-            className="row-span-1 animate-pulse relative inset-10 inline-flex h-12 w-5/6 inline-flex items-center rounded-md border border-transparent px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-white focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 lg:w-1/3 lg:justify-self-end lf:mt-10"
-          >
-            <a
-              href={courseResources.ChatButton.link}
-              className="inline-flex h-max w-max space-x-2"
-            >
-              <ChatBubbleOvalLeftEllipsisIcon className="-ml-1 self-center text-orange-600 h-12 w-12" />
-              <h3 className="text-md text-black text-left self-center leading-none uppercase">
-                {courseResources.ChatButton.text}
-              </h3>
-            </a>
-          </button>
+          <StartChatWithQuestionButton
+            question={courseResources.ChatButton.text}
+          />
         </div>
 
         <div></div>
