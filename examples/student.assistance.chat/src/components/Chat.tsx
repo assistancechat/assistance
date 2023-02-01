9ebcd471b7cc8fba4665582f785dc05d9463b3ab
Kennie12345 <52986783+Kennie12345@users.noreply.github.com>
2023-02-01 21:26:51 +1100

    added autoscroll, widened the send button, cleaned double entries for classNames in Hero, added lazy load to all pages below moreinfo

--- examples/student.assistance.chat/src/components/Chat.tsx
+++ examples/student.assistance.chat/src/components/Chat.tsx
@@ -19,258 +19,274 @@ import {
   ChangeEvent,
   FormEvent,
   useEffect,
-} from "react";
+  useRef
+} from 'react'
 
-import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { PaperAirplaneIcon } from '@heroicons/react/24/solid'
+import { AcademicCapIcon } from '@heroicons/react/24/solid'
 
-import jwt_decode from "jwt-decode";
-import { GoogleLogin, CredentialResponse } from "@react-oauth/google";
+import jwt_decode from 'jwt-decode'
+import { GoogleLogin, CredentialResponse } from '@react-oauth/google'
 
 import {
   ChatContext,
   MessageHistoryItem,
-  MessageHistory,
-} from "@/providers/chat";
+  MessageHistory
+} from '@/providers/chat'
 
-import { callChatApi } from "@/utilities/call-api";
-import { mostRecentChatIsClient } from "@/utilities/flow";
+import { callChatApi } from '@/utilities/call-api'
+import { mostRecentChatIsClient } from '@/utilities/flow'
 
-import ProfilePicture from "@/components/atoms/ProfilePicture";
+import ProfilePicture from '@/components/atoms/ProfilePicture'
 
 const epochToTimestamp = (epoch: number) => {
-  return new Date(epoch).toLocaleString();
-};
+  return new Date(epoch).toLocaleString()
+}
 
 function ChatHistory() {
-  const { chatData } = useContext(ChatContext);
+  const { chatData } = useContext(ChatContext)
+  
+  // Scroll to bottom of chat history when new messages are added
+  const messagesEndRef = useRef(null)
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: 'smooth' })
+  }  
+
+  useEffect(scrollToBottom, [chatData.messageHistory])
 
   const renderChatHistory = () => {
     return chatData.messageHistory.map(
       ({ message, originator, timestamp }, index) => {
-        const timestampAsString = epochToTimestamp(timestamp);
-        const name = chatData.originatorNames[originator];
+        const timestampAsString = epochToTimestamp(timestamp)
+        const name = chatData.originatorNames[originator]
 
         return (
           <div
             key={index}
             className={`flex ${
-              originator === "client" ? "justify-end" : "justify-start"
+              originator === 'client' ? 'justify-end' : 'justify-start'
             } mb-4`}
           >
-            <div className="flex flex-col items-end">
-              <div className="flex items-center">
-                <span className="text-xs text-gray-400 mr-2">
+            <div className='flex flex-col items-end'>
+              <div className='flex items-center'>
+                <span className='text-xs text-gray-400 mr-2'>
                   {timestampAsString}
                 </span>
-                <span className="text-xs text-gray-400">{name}</span>
+                <span className='text-xs text-gray-400'>{name}</span>
               </div>
-              <div className="flex flex-col items-end">
+              <div className='flex flex-col items-end'>
                 <div
                   className={`py-2 px-4 rounded-xl rounded-br-none ${
-                    originator === "client"
-                      ? "bg-orange-400 text-white"
-                      : "bg-gray-400 text-white"
+                    originator === 'client'
+                      ? 'bg-orange-400 text-white'
+                      : 'bg-gray-400 text-white'
                   } max-w-xs`}
                 >
                   {message
                     .replaceAll(
-                      "{agent_name}",
-                      chatData.originatorNames["agent"]
-                        ? chatData.originatorNames["agent"]
-                        : "agent"
+                      '{agent_name}',
+                      chatData.originatorNames['agent']
+                        ? chatData.originatorNames['agent']
+                        : 'agent'
                     )
                     .replaceAll(
-                      "{client_name}",
-                      chatData.originatorNames["client"]
-                        ? chatData.originatorNames["client"]
-                        : "client"
+                      '{client_name}',
+                      chatData.originatorNames['client']
+                        ? chatData.originatorNames['client']
+                        : 'client'
                     )}
                 </div>
                 <ProfilePicture originator={originator} />
               </div>
             </div>
           </div>
-        );
-      }
-    );
-  };
+        )
+      } 
+    )
+  }
+
+  useEffect(() => {
+    scrollToBottom()
+  }, [chatData.messageHistory]
+  )
 
   return (
-    <div className="flex-1 max-h-96 overflow-scroll">
-      <div className="flex flex-col h-full">{renderChatHistory()}</div>
+    <div className='flex-1 max-h-96 overflow-scroll'>
+      <div className='flex flex-col h-full'>{renderChatHistory()}</div>
+      <div ref={messagesEndRef} />
     </div>
-  );
+  )
 }
 
 type GoogleTokenIdData = {
-  picture: string;
-  given_name: string;
-};
+  picture: string
+  given_name: string
+}
 
 function Login() {
-  const { chatData, setChatData } = useContext(ChatContext);
-  const [loginVisible, setLoginVisible] = useState(true);
+  const { chatData, setChatData } = useContext(ChatContext)
+  const [loginVisible, setLoginVisible] = useState(true)
 
   const handleCredentialResponse = async (
     credentialResponse: CredentialResponse
   ) => {
-    const token = credentialResponse.credential;
+    const token = credentialResponse.credential
 
-    console.log(token);
+    console.log(token)
 
     if (token == undefined) {
-      return;
+      return
     }
 
-    const decoded = jwt_decode(token) as GoogleTokenIdData;
+    const decoded = jwt_decode(token) as GoogleTokenIdData
 
-    console.log(decoded);
+    console.log(decoded)
 
-    const profilePictureUrl = decoded["picture"];
-    const clientName = decoded["given_name"];
+    const profilePictureUrl = decoded['picture']
+    const clientName = decoded['given_name']
 
-    chatData.originatorNames["client"] = clientName;
-    chatData.originatorProfilePictureUrls["client"] = profilePictureUrl;
+    chatData.originatorNames['client'] = clientName
+    chatData.originatorProfilePictureUrls['client'] = profilePictureUrl
 
-    chatData.googleIdToken = token;
+    chatData.googleIdToken = token
 
-    let messageHistoryToAppend: MessageHistory;
+    let messageHistoryToAppend: MessageHistory
     if (chatData.pendingQuestion === null) {
       messageHistoryToAppend = [
         {
-          originator: "agent",
+          originator: 'agent',
           message: `Hi {client_name}, it's great to meet you! Thank you for signing in. How can I help you today?`,
-          timestamp: Date.now(),
-        },
-      ];
+          timestamp: Date.now()
+        }
+      ]
     } else {
       messageHistoryToAppend = [
         {
-          originator: "agent",
+          originator: 'agent',
           message: `Hi {client_name}, it's great to meet you! Thank you for signing in.`,
-          timestamp: Date.now(),
+          timestamp: Date.now()
         },
         {
-          originator: "client",
+          originator: 'client',
           message: chatData.pendingQuestion,
-          timestamp: Date.now(),
-        },
-      ];
+          timestamp: Date.now()
+        }
+      ]
 
-      chatData.pendingQuestion = null;
+      chatData.pendingQuestion = null
     }
 
     const updatedMessageHistory = [
       ...chatData.messageHistory,
-      ...messageHistoryToAppend,
-    ];
+      ...messageHistoryToAppend
+    ]
 
     const updatedChatData = {
       ...chatData,
-      messageHistory: updatedMessageHistory,
-    };
+      messageHistory: updatedMessageHistory
+    }
 
-    setChatData(updatedChatData);
+    setChatData(updatedChatData)
 
     if (mostRecentChatIsClient(updatedChatData)) {
-      await callChatApi(updatedChatData, setChatData);
+      await callChatApi(updatedChatData, setChatData)
     }
-  };
+  }
 
   useEffect(() => {
-    setLoginVisible(chatData.googleIdToken == null);
-  }, [chatData]);
+    setLoginVisible(chatData.googleIdToken == null)
+  }, [chatData])
 
   if (loginVisible) {
     return (
-      <div className="max-w-xs m-auto pb-6">
+      <div className='max-w-xs m-auto pb-6'>
         <GoogleLogin
           onSuccess={handleCredentialResponse}
           onError={() => {
-            console.log("Login Failed");
+            console.log('Login Failed')
           }}
         />
       </div>
-    );
+    )
   }
 
-  return <></>;
+  return <></>
 }
 
 function ChatInput() {
-  const { chatData, setChatData } = useContext(ChatContext);
-  const [message, setMessage] = useState("");
+  const { chatData, setChatData } = useContext(ChatContext)
+  const [message, setMessage] = useState('')
 
   const addNewMessage = (message: string) => {
     const newMessageHistoryItem: MessageHistoryItem = {
-      originator: "client",
+      originator: 'client',
       message: message,
-      timestamp: Date.now(),
-    };
+      timestamp: Date.now()
+    }
 
     const updatedMessageHistory = [
       ...chatData.messageHistory,
-      newMessageHistoryItem,
-    ];
+      newMessageHistoryItem
+    ]
 
     const updatedChatData = {
       ...chatData,
-      messageHistory: updatedMessageHistory,
-    };
+      messageHistory: updatedMessageHistory
+    }
 
-    setChatData(updatedChatData);
-    callChatApi(updatedChatData, setChatData);
-  };
+    setChatData(updatedChatData)
+    callChatApi(updatedChatData, setChatData)
+  }
 
   const handleMessageInput = (event: ChangeEvent<HTMLInputElement>) => {
-    setMessage(event.target.value);
-  };
+    setMessage(event.target.value)
+  }
 
   const handleMessageSubmit = (event: MouseEvent<HTMLButtonElement>) => {
-    event.preventDefault();
-    addNewMessage(message);
-    setMessage("");
-  };
+    event.preventDefault()
+    addNewMessage(message)
+    setMessage('')
+  }
 
   //trying to prevent the user from submitting form if the input is empty
   const preventFormSubmission = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-  };
+    event.preventDefault()
+  }
 
   return (
-    <div className="flex items-center justify-between p-1 border-gray-200">
-      <form className="flex w-full" onSubmit={preventFormSubmission}>
-        <div className="flex w-full items-center">
+    <div className='flex items-center justify-between p-1 border-gray-200'>
+      <form className='flex w-full' onSubmit={preventFormSubmission}>
+        <div className='flex w-full bg-gray-800 items-center rounded-lg'>
           <input
-            type="text"
-            className="w-full px-4 py-2 border border-gray-200 rounded-l-md focus:outline-none focus:border-orange-600"
-            placeholder="Ask us about enrolment or application ..."
+            type='text'
+            className='w-full px-4 py-2 border border-gray-200 rounded-l-md focus:outline-none focus:border-orange-600'
+            placeholder='Ask us about enrolment or application ...'
             value={message}
             onChange={handleMessageInput}
             disabled={mostRecentChatIsClient(chatData)}
           />
           <button
-            type="submit"
-            className="bg-gray-800 rounded-r-md focus:ring-offset-2 hover:bg-orange-400 focus:ring-white"
+            type='submit'
+            className='bg-gray-800 w-12 justify-center h-full flex rounded-r-lg focus:ring-offset-2 hover:bg-orange-400 focus:ring-white'
             onClick={handleMessageSubmit}
-            disabled={message === "" || mostRecentChatIsClient(chatData)}
+            disabled={message === '' || mostRecentChatIsClient(chatData)}
           >
-            <PaperAirplaneIcon className="w-12 h-10 pt-2 pb-2 animate-pulse text-white hover:text-gray-800" />
+            <PaperAirplaneIcon className='h-6 w-6 self-center text-white' />
           </button>
         </div>
       </form>
     </div>
-  );
+  )
 }
 
 function Chat() {
   return (
-    <div className="flex flex-col flex-1 h-96 bg-gray-300">
+    <div className='flex flex-col flex-1 h-96 bg-gray-300'>
       <ChatHistory />
       <Login />
       <ChatInput />
     </div>
-  );
+  )
 }
 
-export default Chat;
+export default Chat
