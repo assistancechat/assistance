96883f1f09f5a62c1088092af7c36b05dae2d1a6
Kennie12345 <52986783+Kennie12345@users.noreply.github.com>
2023-02-08 13:36:40 +1100

    form thank you positioned on the correct z-axis, close button works

--- examples/student.assistance.chat/src/components/FormThankYou.tsx
+++ examples/student.assistance.chat/src/components/FormThankYou.tsx
@@ -12,7 +12,7 @@ export function FormThankYou(props: {
       {/* Global notification live region, render this permanently at the end of the document */}
       <div
         aria-live="assertive"
-        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
+        className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start z-50 sm:p-6"
       >
         <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
           {/* Notification panel, dynamically insert this into the live region when it needs to be displayed */}
