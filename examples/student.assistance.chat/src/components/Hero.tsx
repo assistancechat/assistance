69ddf4b6d0582f985668a5908755e9d536bc7ce6
Ken Cheung <kencheung@MacBook-Pro.local>
2023-02-14 22:57:38 +0700

    added aged care, refactored Hero pages so that typing is more consistent, changed the agedCare.json files

--- examples/student.assistance.chat/src/components/Hero.tsx
+++ examples/student.assistance.chat/src/components/Hero.tsx
@@ -12,72 +12,122 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import React from "react";
-import { ArrowDownCircleIcon } from "@heroicons/react/24/solid";
+import { useState, useEffect, useRef } from "react";
 
 import StartChatWithQuestionButton from "@/components/atoms/StartChatWithQuestionButton";
+import Typed from "typed.js";
 
-type HeroProps = {
+type HeroOpeningProps = {
   portraitPicture: string;
   landscapePicture: string;
   alt: string;
-  learnButtonLink: string;
-  question: string;
-  learnButtonText: string;
-  headLine: string;
-  subHeading: string;
   chatButtonText: string;
 };
 
-export default function Hero(props: HeroProps) {
+export default function HeroOpening(props: HeroOpeningProps) {
+  const headlineConfig = {
+    strings: [
+      "<u>Australia</u>",
+      "<u>Australian Health </u>",
+      "<u>Nursing</u>",
+      "<u>Aged Care</u>",
+    ],
+    typeSpeed: 5,
+    backSpeed: 25,
+    backDelay: 1200,
+    loop: true,
+    loopCount: 1,
+    showCursor: false,
+    bindInputFocusEvents: true,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500,
+  };
+
+  const subHeadingConfig = {
+    strings: [
+      "Opportunities are limited",
+      "Don't wait and bury your talent",
+      "ASK US TODAY."
+    ],
+    typeSpeed: 15,
+    backSpeed: 10,
+    backDelay: 1350,
+    startDelay: 8000,
+    loop: true,
+    loopCount: 1,
+    showCursor: false,
+    fadeOut: false,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500,
+  };
+
+  const headlineRef = useRef() as React.MutableRefObject<HTMLHeadingElement>;
+  const [headline, setHeadline] = useState<Typed>();
+
+  const subHeadingRef = useRef() as React.MutableRefObject<HTMLHeadingElement>;
+  const [subHeading, setSubHeading] = useState<Typed>();
+
+  const constTypedEffects = [
+    {
+      ref: headlineRef,
+      set: setHeadline,
+      config: headlineConfig,
+    },
+    {
+      ref: subHeadingRef,
+      set: setSubHeading,
+      config: subHeadingConfig,
+    },
+  ];
+
+  constTypedEffects.map((items) => {
+    useEffect(() => {
+      const typed = new Typed(items.ref.current, items.config);
+      items.set(typed);
+      return () => {
+        typed.destroy();
+      };
+    }, []);
+  });
+
   return (
     <div className="w-screen h-screen">
       <img
-        className="absolute opacity-30 -z-10 w-screen h-screen lg:hidden"
+        className="absolute opacity-30 -z-10 w-screen h-screen md:hidden"
         src={props.portraitPicture}
         alt={props.alt}
       />
       <img
-        className="absolute opacity-30 -z-10 w-screen h-screen hidden lg:block"
+        className="absolute opacity-30 -z-10 w-screen h-screen hidden md:block"
         src={props.landscapePicture}
         alt={props.alt}
       />
       <div className="grid grid-rows-6 w-screen h-screen">
-        <button
-          type="button"
-          className="row-span-2 left-10 top-40 bg-orange-400 md:top-30 h-12 w-3/4 relative inline-flex items-center rounded-md border border-transparent px-4 py-2 shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 lg:w-1/4"
-        >
-          <a
-            href={props.learnButtonLink}
-            rel="noreferrer"
-            target="_blank"
-            className="inline-flex h-max w-max space-x-2"
-          >
-            <ArrowDownCircleIcon className="h-10 w-10 self-center text-white" />
-            <h3 className="text-base font-medium text-white leading-none uppercase place-self-center ">
-              {props.learnButtonText}
-            </h3>
-          </a>
-        </button>
-        <div className="space-y-6">
-          <div className="row-span-4 relative left-5 sm:left-10 w-4/5 space-y-4 ">
-            <h1 className="pt-4 text-5xl tracking-normalleading-none border-orange-400 border-t-4 ">
-              {props.headLine}
-            </h1>
-            <h3 className="text-xl w-4/5 tracking-wide leading-tight font-light text-gray-800">
-              {props.subHeading}
-            </h3>
+        <div className="row-span-1 lg:row-span-2"></div>
+        <div className="space-y-6 w-screen">
+          <div className="row-span-4 relative w-screen space-y-4 ">
+            <hr className="border-orange-400 border-2 w-10/12 ml-10" />
+            <h1 className = "text-5xl pl-10 tracking-normal leading-none"> Discover God's Purpose for you in </h1>
+            <span
+              className="text-5xl pl-10 tracking-normal leading-none"
+              ref={headlineRef}
+            />
+            <h3
+              className="text-xl pl-10 pr-10 tracking-wide leading-tight font-light text-gray-800 w-screen"
+              ref={subHeadingRef}
+            />
           </div>
 
           <StartChatWithQuestionButton
             question={props.chatButtonText}
-            buttonClassName="row-span-1 relative bg-orange-400 inset-10 inline-flex w-5/6 rounded-md py-1 px-4 shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 lg:w-1/3 lg:justify-self-end lf:mt-10 space-x-4"
-            bubbleClassName="-ml-1 animate-pulse self-center text-white h-12 w-12"
-            textClassName="text-base font-medium text-white uppercase leading-none text-left place-self-center"
+            buttonClassName="row-span-1 inset-10 relative bg-gray-800 inline-flex w-9/12 rounded-md py-1 px-4 shadow-sm hover:bg-orange-400 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 lg:w-1/3 lg:justify-self-end space-x-4"
+            bubbleClassName="-ml-1 self-center text-white h-12 w-12"
+            textClassName="text-sm font-medium text-white uppercase leading-none text-left place-self-center"
           />
         </div>
 
-        <div></div>
+        <div className="w-screen"></div>
       </div>
     </div>
   );
