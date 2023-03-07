5db3157f0212da3cf4da6055c891dc713c05cfa5
Simon Biggs <simon.biggs@radiotherapy.ai>
2023-03-08 07:43:15 +1100

    remove typescript libs

--- src/typescript/browser-extensions/assistance/loader/gmail-js-loader.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-const GmailFactory = require("gmail-js");
-const jQuery = require("jquery");
-
-window._gmailjs = window._gmailjs || new GmailFactory.Gmail(jQuery);
--- src/typescript/libraries/assistance/src/client/models/ChatResponse.ts
+++
@@ -1,8 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export type ChatResponse = {
-    agent_message: string;
-    assistance_token: string;
-};
