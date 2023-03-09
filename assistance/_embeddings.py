381e18a4253b53db49d2a9b8ab60f208f50684eb
Simon Biggs <me@simonbiggs.net>
2023-03-09 16:33:14 +1100

    get prioritised Q and A transcript from multiple Qs

--- assistance/_embeddings.py
+++ assistance/_embeddings.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+import collections
 import torch
 import asyncio
 from asyncache import cached
@@ -22,8 +22,37 @@ from cachetools import LRUCache
 from assistance._openai import get_embedding
 
 
-async def get_top_questions_and_answers(openai_api_key, faq_data, query, k=3):
-    query_embedding = await _get_cuda_embedding(query, openai_api_key=openai_api_key)
+async def get_top_questions_and_answers_text(openai_api_key, faq_data, queries, k=3):
+    all_most_relevant_results = await _get_top_questions_and_answers(
+        openai_api_key, faq_data, queries, k=k
+    )
+
+    collected_q_and_a_strings_with_score = collections.defaultdict(list)
+
+    for most_relevant_results in all_most_relevant_results:
+        for item in most_relevant_results:
+            q_and_a_string = (
+                f"Q: {item['question'].strip()}\nA: {item['answer'].strip()}"
+            )
+
+            collected_q_and_a_strings_with_score[q_and_a_string].append(item["score"])
+
+    strings_and_scores = []
+    for q_and_a_string, scores in collected_q_and_a_strings_with_score.items():
+        sqrt_sum_of_square_scores = (sum(score**2 for score in scores)) ** 0.5
+        strings_and_scores.append((q_and_a_string, sqrt_sum_of_square_scores))
+
+    strings_and_scores.sort(key=lambda x: x[1], reverse=True)
+    collected_q_and_a_strings = [x[0] for x in strings_and_scores]
+
+    return "\n\n".join(collected_q_and_a_strings)
+
+
+async def _get_top_questions_and_answers(openai_api_key, faq_data, queries, k=3):
+    queries = tuple(queries)
+    queries_embedding = await _get_cuda_embeddings(
+        queries, openai_api_key=openai_api_key
+    )
 
     all_questions: tuple[str, ...] = tuple(
         [item["question"] for item in faq_data["items"]]
@@ -31,36 +60,40 @@ async def get_top_questions_and_answers(openai_api_key, faq_data, query, k=3):
     embeddings = await _get_cuda_embeddings(
         all_questions, openai_api_key=openai_api_key
     )
-    indices, scores = top_k_embeddings(query_embedding, embeddings, k)
+    all_queries_indices, all_queries_scores = top_k_embeddings(
+        queries_embedding, embeddings, k
+    )
 
-    most_relevant_results = [faq_data["items"][i] for i in indices]
-    for item, score in zip(most_relevant_results, scores):
-        item["relevance_score"] = score
+    all_most_relevant_results = []
 
-    return [faq_data["items"][i] for i in indices]
+    for indices, scores in zip(all_queries_indices, all_queries_scores):
+        most_relevant_results = [faq_data["items"][i] for i in indices]
+        for item, score in zip(most_relevant_results, scores):
+            item["score"] = score
+
+        all_most_relevant_results.append(most_relevant_results)
+
+    return all_most_relevant_results
 
 
-def top_k_embeddings(query, embeddings, k):
+def top_k_embeddings(queries, embeddings, k):
     k = torch.tensor(k, device="cuda")
-    cosine_similarity, index = _top_k_embeddings(query, embeddings, k)
+    cosine_similarity, index = _top_k_embeddings(queries, embeddings, k)
 
     index = index.tolist()
     cosine_similarity = cosine_similarity.tolist()
 
-    assert len(index) == 1
-    assert len(cosine_similarity) == 1
-
-    return index[0], cosine_similarity[0]
+    return index, cosine_similarity
 
 
 @torch.jit.script  # type: ignore
-def _top_k_embeddings(query, embeddings, k):
-    transpose_query = query.T
+def _top_k_embeddings(queries, embeddings, k):
+    transpose_queries = queries.T
     embeddings_norm = torch.linalg.norm(embeddings, dim=1, keepdim=True)
-    query_norm = torch.linalg.norm(transpose_query, dim=0, keepdim=True)
+    query_norm = torch.linalg.norm(transpose_queries, dim=0, keepdim=True)
 
     cosine_similarity = (
-        (embeddings @ transpose_query) / (embeddings_norm @ query_norm)
+        (embeddings @ transpose_queries) / (embeddings_norm @ query_norm)
     ).T
 
     return torch.topk(cosine_similarity, k)
