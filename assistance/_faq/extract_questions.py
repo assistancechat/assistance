f0db95f2fa8d8a2cf85cbd73edd112662527038c
Simon Biggs <me@simonbiggs.net>
2023-03-15 12:30:37 +1100

    include step-by-step for question extraction

--- assistance/_faq/extract_questions.py
+++ assistance/_faq/extract_questions.py
@@ -75,27 +75,33 @@ PROMPT = textwrap.dedent(
 
         [
             {{
+                "think step by step for question and its context": "<step by step reasoning>",
                 "question": "<first question>",
-                "context": "<any relevant context from the email transcript>",
+                "context": "<any relevant question context from the email transcript>",
+                "think step by step for extracted answer": "<step by step reasoning>",
                 "extracted answer": "<The answer given in the transcript>",
-                "think step by step": "<step by step reasoning>",
+                "think step by step for verification questions": "<step by step reasoning>",
                 "has the user's question been answered?": <true or false>,
                 "was this question asked after the given answer?": <true or false>
             }},
             {{
+                "think step by step for question and its context": "<step by step reasoning>",
                 "question": "<first question>",
-                "context": "<any relevant context from the email transcript>",
+                "context": "<any relevant question context from the email transcript>",
+                "think step by step for extracted answer": "<step by step reasoning>",
                 "extracted answer": "<The answer given in the transcript>",
-                "think step by step": "<step by step reasoning>",
+                "think step by step for verification questions": "<step by step reasoning>",
                 "has the user's question been answered?": <true or false>,
                 "was this question asked after the given answer?": <true or false>
             }},
             ...
             {{
+                "think step by step for question and its context": "<step by step reasoning>",
                 "question": "<first question>",
-                "context": "<any relevant context from the email transcript>",
+                "context": "<any relevant question context from the email transcript>",
+                "think step by step for extracted answer": "<step by step reasoning>",
                 "extracted answer": "<The answer given in the transcript>",
-                "think step by step": "<step by step reasoning>",
+                "think step by step for verification questions": "<step by step reasoning>",
                 "has the user's question been answered?": <true or false>,
                 "was this question asked after the given answer?": <true or false>
             }}
